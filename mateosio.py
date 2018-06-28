aae2f7f62bc427f2a109523ca2e1f318faf4d97a
danimateos <dmateos.sanmartin@gmail.com>
2018-06-28 13:05:01 -0400

    Helper function for confusion matrix

--- mateosio.py
+++ mateosio.py
@@ -1,10 +1,11 @@
 from itertools import chain
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 def plot_training_histories(*histories):
-
+    '''Abstract away the plotting of training histories'''
     
-
     figure, axes = plt.subplots(2,1)
     figure.set_size_inches(8,8)
     
@@ -22,3 +23,19 @@ def plot_training_histories(*histories):
         axes[ax].legend()
     
     return figure
+
+
+def confusion_matrix(model, validation_generator):
+    '''Plot the confusion matrix, return precision and recall'''
+    
+    predictions = model.predict_generator(validation_generator)
+
+    C = confusion_matrix(validation_generator.classes, predictions > .5)
+    ax = sns.heatmap(C, annot=True, square=True)
+    ax.set_ylabel('True class')
+    ax.set_xlabel('Predicted class')
+
+    precision = C[0,0] / (C[0,0] + C[1,0])
+    recall = C[0][0] / (C[0,0] + C[0,1])
+
+    return ax, precision, recall
